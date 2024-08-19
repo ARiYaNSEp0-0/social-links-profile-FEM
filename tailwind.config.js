f3bda808fe8936771d97c9fd7ffb5ae2d44d5224
AriyanSep0_0 <ariansepahi.v8@gmail.com>
2024-08-20 01:21:51 +0330

    :bug: fix vite bundling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./index.html"],
+  content: ["./src/*.html"],
   theme: {
     extend: {
       screen: {
