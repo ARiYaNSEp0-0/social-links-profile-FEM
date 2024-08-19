afd4a110a81fdc9a6b975bd25df069d65ee62850
AriyanSep0_0 <ariansepahi.v8@gmail.com>
2024-08-19 23:29:21 +0330

    :tada: Adding design system to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      fontFamily: {
+        serif: ['"Inter"', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        green: {
+          DEFAULT: "#c5f82a",
+        },
+        gray: {
+          700: "#333333",
+          800: "#1f1f1f",
+          900: "#141414",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
